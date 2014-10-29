85c80d0abf84004c813718da6d35ae0fdc0b800f
Tobias Sailer <tobias.sailer@gmail.com>
2014-10-29 11:21:12 +0100

    Abschluss Sprint4

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,9 @@
-console.log('ich bon js');
+/*jshint browser:true ,devel:true*/
+
+(function(window, document, undefined) {
+  "use strict";
+
+  var test = 'Test string';
+
+  console.log('ich bin js und das ist ein %s', test);
+})(window, document);
