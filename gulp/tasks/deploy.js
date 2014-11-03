03f0ae38b993ceacc774c403429868997242cc96
Tobias Sailer <tobias.sailer@gmail.com>
2014-11-03 12:07:05 +0100

    Sprint5 Zwischenstand

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,12 +1,12 @@
-var deploy = require('gulp-gh-pages');
-var gulp       = require('gulp');
+var deploy = require('gulp-gh-pages'),
+    gulp       = require('gulp');
 
 
 var options = {
     branch: "master"
   };
 
-gulp.task('deploy',['compile'], function () {
+gulp.task('deploy', 'Schiebt alles auf GITHUB',['compile'], function () {
     return gulp.src('./public/**/*')
         .pipe(deploy(options));
 });
--- gulp/tasks/newPost.js
+++ gulp/tasks/newPost.js
@@ -1,7 +1,7 @@
 var gulp         = require('gulp'),
+    prompt       = require('gulp-prompt'),
     cwd          = process.cwd(),
     jeditor      = require("gulp-json-editor"),
-    prompt       = require('gulp-prompt'),
     rename       = require('gulp-rename'),
     template     = require('gulp-template'),
     padNum       = require(cwd + '/gulp/util/helpers').padNum,
@@ -11,33 +11,64 @@ var gulp         = require('gulp'),
     dataInfo     = require(dataFolder+dataFile),
     today        = new Date().toISOString().slice(0,10),
     newPostId    = padNum('00000',Object.keys(dataInfo).length+1),
-    postName     = newPostId +'_'+(today);
+    postName     = newPostId +'_'+(today),
+    userInput    = 'userInput';
 
-// mit diesem Task erstellen wir einen neuen Pos
-    console.log(__dirname);
-    console.log(process.cwd());
-gulp.task('newpost','erstellt einen neuen Post',function () {
 
-    // JSON schreiben
-    gulp.src(dataFolder+dataFile)
-      .pipe(jeditor(function(json) {
-        json[postName] = {
-                          "title": "POST " + newPostId,
-                          "author": "Tobias Sailer",
-                          "email": "tobias.sailer@gmail.com",
-                          "date": today,
-                          "type": "post"
-                        };
-        return json; // must return JSON object.
-      }))
-      .pipe(gulp.dest(dataFolder));
+var fragen = [
+                {
+                  type: "input",
+                  name: "ueberschrift",
+                  message: "Überschrift"
+                },
+                {
+                  type: "input",
+                  name: "tags",
+                  message: "Taglist",
+                  default: function () { return "Post"; }
+                }
+              ];
 
-    // jetzt die MD Datei
-     gulp.src(dataFolder + dataTemplate)
-      .pipe(template({ueberschrift: today}))
-      .pipe(rename(postName + '.md'))
-      .pipe(gulp.dest(dataFolder));
+// MD Datei klonen und erstellen
+gulp.task('readwritejson',false ,['userinput'], function () {
+   return gulp.src(dataFolder + dataTemplate)
+            .pipe(template({ueberschrift: userInput.ueberschrift}))
+            .pipe(rename(postName + '.md'))
+            .pipe(gulp.dest(dataFolder));
+});
 
-    console.log('Datei erstellt: ' + postName);
 
+// JSON schreiben
+gulp.task('writepostfile',false, ['userinput'], function () {
+    return gulp.src(dataFolder+dataFile)
+            .pipe(jeditor(function(json) {
+              var newPostJson = {
+                                "title": userInput.ueberschrift,
+                                "author": "Tobias Sailer",
+                                "email": "tobias.sailer@gmail.com",
+                                "date": today,
+                                "type": "post",
+                                "tags": userInput.tags
+                              };
+              json[postName] = newPostJson;
+              console.log('JSON erstellt: ' + JSON.stringify(newPostJson,null,'..'));
+              return json; // must return JSON object.
+            }))
+            .pipe(gulp.dest(dataFolder));
+});
+
+
+// Zuerst mal nach dem Titel fragen ..
+gulp.task('userinput',function () {
+  return gulp.src(dataFolder+dataFile,{read:false})
+              .pipe(prompt.prompt(fragen,
+                    function(antworten) {
+                      userInput = antworten;
+                    }
+                ));
+});
+
+// mit diesem Task erstellen wir einen neuen Post
+gulp.task('newpost','erstellt einen neuen POST' ,['userinput','readwritejson','writepostfile'], function () {
+    // console.log('newpost ' + userInput);
 });
